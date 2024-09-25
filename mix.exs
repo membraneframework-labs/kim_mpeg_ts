20056790e688cd9ee7e0baa6c2a9e7335fee2fbf
Daniel Morandini <danielmorandini@me.com>
2024-09-25 12:28:42 +0200

    Remove hex name

--- mix.exs
+++ mix.exs
@@ -4,11 +4,12 @@ defmodule MPEG.TS.MixProject do
   def project do
     [
       app: :kim_mpeg_ts,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       source_url: "https://github.com/kim-company/kim_mpeg_ts",
+      name: "MPEG.TS",
       description: description(),
       package: package(),
       deps: deps()
@@ -24,7 +25,7 @@ defmodule MPEG.TS.MixProject do
 
   defp package do
     [
-      organization: "kim_company",
+      maintainers: ["KIM Keep In Mind"],
       files: ~w(lib mix.exs README.md LICENSE),
       licenses: ["Apache-2.0"],
       links: %{"GitHub" => "https://github.com/kim-company/kim_mpeg_ts"}
@@ -40,7 +41,9 @@ defmodule MPEG.TS.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
